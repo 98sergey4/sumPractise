ac8fcd6ef5fee1b5fe56181bf4069eafbed5c3f1
98sergey4 <s.chyshkala@student.csn.khai.edu>
2018-06-27 06:09:36 +0300

    dsvddhv

--- kvadratnoye_lab2_tp/Tests/Class1.cs
+++ kvadratnoye_lab2_tp/Tests/Class1.cs
@@ -26,7 +26,42 @@ namespace Tests
             expected = 0.25;
             Assert.That(test.result1, Is.EqualTo(expected));
         }
-       
+        [Test]
+        public void Test11()
+        {
+            EventArgs e = new EventArgs();
+            Form1 test = new Form1();
+            test.radio_button_obrabotchik.Checked = true;
+            test.textBox1.Text = "4.21";
+            test.textBox2.Text = "10";
+            test.textBox3.Text = "-5.075";
+            double expected = -2.805;
+            test.result1 = 0;
+            test.Button_result(this, e);
+            test.result1 = Math.Round(test.result1, 3);
+            test.result2 = Math.Round(test.result2, 3);
+            Assert.That(test.result2, Is.EqualTo(expected));
+            expected = 0.430;
+            Assert.That(test.result1, Is.EqualTo(expected));
+        }
+        [Test]
+        public void Test111()
+        {
+            EventArgs e = new EventArgs();
+            Form1 test = new Form1();
+            test.radio_button_obrabotchik.Checked = true;
+            test.textBox1.Text = "7";
+            test.textBox2.Text = "-20.2";
+            test.textBox3.Text = "-12";
+            double expected = -0.506;
+            test.result1 = 0;
+            test.Button_result(this, e);
+            test.result1 = Math.Round(test.result1, 3);
+            test.result2 = Math.Round(test.result2, 3);
+            Assert.That(test.result2, Is.EqualTo(expected));
+            expected = 3.391;
+            Assert.That(test.result1, Is.EqualTo(expected));
+        }
         [Test]
         public void Test2()
         {
